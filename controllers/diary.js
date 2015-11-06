dc890d3debb2df5989a5d954c855efd4b00b21bb
theking9985 <theking9985@yahoo.com>
2015-11-05 19:53:19 -0800

    add all data to log table

--- controllers/diary.js
+++ controllers/diary.js
@@ -8,22 +8,7 @@ router.get('/', function(req, res) {
 	res.render('diary', {goal: goal});
 });
 
-
-// //render diary.ejs with data from table in progress
-// router.get('/',function (req, res) {
-// 	db.log.findAll( {
-// 		include: [db.nutrient],
-// 		where: {
-// 			userId: req.session.user,
-// 			date: req.session.currentDate
-// 		}}).then(function(logs) {
-// 			console.log(logs);
-// 		});
-
-// });
-
 // diary of date page
-
 router.get('/:date', function(req, res) {
 	console.log("user: "+req.session.user+", date: "+req.params.date);
 	db.log.findAll( {
@@ -33,38 +18,14 @@ router.get('/:date', function(req, res) {
 			date: req.params.date
 		}}).then(function(logs) {
 			if (logs.length) {
-				// render response
+				res.render('log', {logs: logs});
+				
 				console.log(logs[0].nutrient);
 			} else {
 				console.log("No logs found!");
 			}
 		});
 	});
-
-
-
-
-
-
-
-
-
-// //example from class
-// router.get('/', function(req, res) {
-//   db.favorite.findAll({
-//     order: 'title ASC'
-//   }).then(function(favorites) {
-//     res.render('favorites/index', {favorites: favorites});
-//   });
-// });
-
-// router.get('/', function(req, res) {
-//   db.tag.findAll({
-//     include: [db.favorite]
-//   }).then(function(tags) {
-//     res.render('tags/index', {tags: tags});
-//   });
-// });
 
 //working post
 router.post('/', function(req, res) {
@@ -77,10 +38,5 @@ router.post('/', function(req, res) {
 	}
 	res.redirect('/diary');
 });
-
-
-
-
-
 
 module.exports = router;
--- controllers/quantity.js
+++ controllers/quantity.js
@@ -7,7 +7,7 @@ var db = require('../models');
 router.get('/:id', function(req, res) {
 	var id = req.params.id;
 	request(
-		'http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=ejl5haOAJvW06Tlx5EiZ3kuBdKCZEaS0vM53SPsM&nutrients=203&nutrients=204&nutrients=205&ndbno=' + id,
+		'http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=' + process.env.USDA_KEY + '&nutrients=203&nutrients=204&nutrients=205&ndbno=' + id,
 		function(error, response, body) {
 			if (!error && response.statusCode === 200) {
 				var data = JSON.parse(body);
@@ -16,6 +16,8 @@ router.get('/:id', function(req, res) {
 		}
 	)
 });
+
+http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=ejl5haOAJvW06Tlx5EiZ3kuBdKCZEaS0vM53SPsM&nutrients=203&nutrients=204&nutrients=205&ndbno=09041
 
 //input data into nutrient table & log table when click on add food
 router.post('/:id', function( req, res) {
